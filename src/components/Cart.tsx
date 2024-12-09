b1883eea41e99d5ac038ec7961d80f45ffab34cf
ali <aligadit9192@gmail.com>
2024-12-09 23:46:23 +0500

    problem solved

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -4,8 +4,8 @@ import React from 'react'
 const Cart = () => {
   return (
     <div className='flex flex-col items-center justify-center max-w-[1990px] mx-auto mt-[80px]'>
-      <Image src='/CartPage.png' alt='cart' height={632} width={1609}/>
-      
+      <Image src='/cartPage.png' alt='cart' height={632} width={1609}/>
+    
     </div>
   )
 }
