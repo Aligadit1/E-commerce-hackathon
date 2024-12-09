7a7add03b962fd5f265e989736ebec8839870a21
ali <aligadit9192@gmail.com>
2024-12-09 23:14:28 +0500

    update in cart

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -5,6 +5,7 @@ const Cart = () => {
   return (
     <div className='flex flex-col items-center justify-center max-w-[1990px] mx-auto mt-[80px]'>
       <Image src='/CartPage.png' alt='cart' height={632} width={1609}/>
+      
     </div>
   )
 }
